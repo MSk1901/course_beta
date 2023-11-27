816ff669a702dc3ca1a6c465a2a81c6e3c0534fe
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-27 22:48:16 +0300

    change filemode to w+

--- src/logger.py
+++ src/logger.py
@@ -6,6 +6,6 @@ PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "mylog.l
 
 def setup_logger(name: str):
     """Конфигуратор логгера"""
-    logging.basicConfig(filename=PATH, filemode="w", level=logging.DEBUG,
+    logging.basicConfig(filename=PATH, filemode="w+", level=logging.DEBUG,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     return logging.getLogger(name)
