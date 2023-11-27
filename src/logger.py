ccaaf0c99d3d54dd6f6cd61f4e0afc7215cd8646
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-27 21:31:11 +0300

    add setup_logger func

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+import logging
+import os
+
+
+PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "mylog.log")
+
+
+def setup_logger(name: str):
+    """Конфигуратор логгера"""
+    logging.basicConfig(filename=PATH, filemode="w", level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    return logging.getLogger(name)
