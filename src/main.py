44148c2d396136bd2df442cf48ad9d91270a3fdd
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-28 17:42:37 +0300

    refactor by PEP-8

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 import os
 
-
+from src.reports import spending_by_category
+from src.services import search_phone_numbers
 from src.utils import get_data_excel, get_settings, make_listdict
 from src.views import main_json
-from src.services import search_phone_numbers
-from src.reports import spending_by_category
-
 
 FILE_TRANSACTIONS = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.xls")
 FILE_SETTINGS = os.path.join(os.path.dirname(os.path.dirname(__file__)), "user_settings.json")
